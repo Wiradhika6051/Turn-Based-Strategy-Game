dcdc87d704fe0e3ad10309cb0f7a0874b666155b
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2023-06-09 21:21:57 +0700

    init: init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.tbsg.turnbasedstrategygame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.tbsg.turnbasedstrategygame to javafx.fxml;
+    exports com.tbsg.turnbasedstrategygame;
+}
