6b591f93897b00369bda1a46aa963c6865140f94
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2023-06-15 20:35:36 +0700

    feat: make progress bar responsive

--- src/main/java/com/tbsg/turnbasedstrategygame/controllers/LoadingScreenController.java
+++ src/main/java/com/tbsg/turnbasedstrategygame/controllers/LoadingScreenController.java
@@ -1,14 +1,80 @@
 package com.tbsg.turnbasedstrategygame.controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
-public class LoadingScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingScreenController implements Initializable {
+    //public class LoadingScreenController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    ProgressBar progressBar;
+
+    @FXML
+    VBox root;
+
+    private double progressBarWidth;
+    //
+    private double windowWidth;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+//        welcomeText.setText("Welcome to JavaFX Application!");
+        progressBar.setProgress(0.6767);
+    }
+
+//    public double getProgressBarWidth() {
+//        return progressBarWidth;
+//    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        //dapetin screen
+//        Screen screen = Screen.getPrimary();
+//
+//        //dapetin visual bound screen
+//        double screenWidth = screen.getVisualBounds().getWidth();
+//
+//        //set width progress bar
+//        progressBar.setPrefWidth(screenWidth * 0.2);
+//        progressBarWidth = root.getWidth();
+//        System.out.println(progressBarWidth);
+//        progressBar.setPrefWidth(progressBarWidth * 0.5);
+        //tunggu scenenya kemuat
+//        root.sceneProperty().addListener((observable, oldScene, newScene) -> {
+//            if (newScene != null) {
+//                Platform.runLater(() -> {
+//                    //dapetin stage
+////                Stage stage = (Stage) progressBar.getScene().getWindow();
+////                //dapetin widthnya
+////                windowWidth = stage.getWidth();
+//                    windowWidth = newScene.getWidth();
+//                    System.out.println("ahou");
+//                    System.out.println(windowWidth);
+//                    //set width progress bar
+//                    progressBarWidth = windowWidth * 0.5;
+//                });
+//
+//            }
+//        });
+        root.boundsInParentProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                windowWidth = newValue.getWidth();
+                progressBarWidth = windowWidth * 0.8;
+                progressBar.setPrefWidth(progressBarWidth);
+            }
+        });
+
+
     }
 }
